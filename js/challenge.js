9696d4f8121f3abefec74b8a2fbcadc4229fe593
PercTruman <drshawnhart@gmail.com>
2022-04-13 12:36:52 -0500

    almost finished

--- js/challenge.js
+++ js/challenge.js
@@ -5,12 +5,19 @@ const newLikeElement =document.createElement('h2')
 const commentList =document.querySelector('#list')
 const keptCounterValues =[]
 
+function setUpButtons(){
 document.querySelector('#minus').addEventListener('click', minusOne)
 document.querySelector('#plus').addEventListener('click', plusOne)
 document.querySelector('#heart').addEventListener('click', like)
 document.querySelector('#pause').addEventListener('click', pause)
+}
+setUpButtons()
 
 const myCounter = setInterval(countUp, 1000)
+
+
+
+
 
 
 function countUp(){
@@ -30,7 +37,7 @@ function like(){
     const rawInteger = parseInt(newLikeElement.textContent)
 
     if (keptCounterValues.includes(rawInteger)){
-        commentList.innerHTML +=`${newLikeElement.textContent} has been liked ${likeCounter + 1} time(s)`
+        commentList.innerHTML +=`${newLikeElement.textContent} has been liked ${likeCounter + 1} time(s)<br>`
 
     }else {
         keptCounterValues.push(rawInteger)
@@ -39,7 +46,15 @@ function like(){
    }
 
 function pause(){
+    clearInterval(myCounter)
+    document.querySelector('#minus').removeEventListener('click', minusOne)
+    document.querySelector('#plus').removeEventListener('click', plusOne)
+    document.querySelector('#heart').removeEventListener('click', like)
+    document.querySelector('#pause').textContent = 'resume'
+}
 
-
+function resume(){
+    document.querySelector('#pause').addEventListener('click', setUpButtons)
+    setInterval(countUp, 1000)
 }
 
