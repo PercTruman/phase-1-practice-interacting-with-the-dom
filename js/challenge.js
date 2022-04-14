4dbaf62b6ea8fb54287fa1c1eae953f65497813f
PercTruman <drshawnhart@gmail.com>
2022-04-13 20:59:54 -0500

    all buttons working

--- js/challenge.js
+++ js/challenge.js
@@ -4,20 +4,14 @@ let likeCounter = 1
 const newLikeElement =document.createElement('h2')
 const commentList =document.querySelector('#list')
 const keptCounterValues =[]
-
-function setUpButtons(){
-document.querySelector('#minus').addEventListener('click', minusOne)
-document.querySelector('#plus').addEventListener('click', plusOne)
-document.querySelector('#heart').addEventListener('click', like)
-document.querySelector('#pause').addEventListener('click', pause)
-}
-setUpButtons()
-
-const myCounter = setInterval(countUp, 1000)
+let myCounter = setInterval(countUp, 1000)
 
 
 
-
+    document.querySelector('#minus').addEventListener('click', minusOne)
+    document.querySelector('#plus').addEventListener('click', plusOne)
+    document.querySelector('#heart').addEventListener('click', like)
+    document.querySelector('#pause').addEventListener('click', pause)
 
 
 function countUp(){
@@ -37,24 +31,40 @@ function like(){
     const rawInteger = parseInt(newLikeElement.textContent)
 
     if (keptCounterValues.includes(rawInteger)){
-        commentList.innerHTML +=`${newLikeElement.textContent} has been liked ${likeCounter + 1} time(s)<br>`
-
+        commentList.innerHTML +=`${newLikeElement.textContent} has been liked ${likeCounter+1} time(s)<br>`
+        likeCounter+=1
     }else {
+        likeCounter=1
         keptCounterValues.push(rawInteger)
         commentList.innerHTML +=`${newLikeElement.textContent} has been liked ${likeCounter} time(s)<br>`
-    }
+        }
+    
    }
 
 function pause(){
-    clearInterval(myCounter)
+    
+    if (document.querySelector('#pause').textContent === 'pause'){
+        clearInterval(myCounter)
+        console.log(myCounter)
+        document.querySelector('#pause').textContent = 'resume'
+        disableButtons()
+    }else{
+        myCounter = setInterval(countUp, 1000)
+        console.log('hello world')
+        document.querySelector('#pause').textContent = 'pause'
+        document.querySelector('#minus').addEventListener('click', minusOne)
+        document.querySelector('#plus').addEventListener('click', plusOne)
+        document.querySelector('#heart').addEventListener('click', like)
+    }
+}
+
+function disableButtons(){
     document.querySelector('#minus').removeEventListener('click', minusOne)
     document.querySelector('#plus').removeEventListener('click', plusOne)
     document.querySelector('#heart').removeEventListener('click', like)
-    document.querySelector('#pause').textContent = 'resume'
 }
+    
 
-function resume(){
-    document.querySelector('#pause').addEventListener('click', setUpButtons)
-    setInterval(countUp, 1000)
-}
+
+
 
